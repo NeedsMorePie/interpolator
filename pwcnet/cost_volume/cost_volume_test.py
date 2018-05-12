56f102153acf5c7c852f387b91bdfe0a2883f40f
TianXing Li <tianxingli24@gmail.com>
2018-05-11 23:05:19 -0400

    Removed clutter code. Adding batch test.

--- pwcnet/cost_volume/cost_volume_test.py
+++ pwcnet/cost_volume/cost_volume_test.py
@@ -68,14 +68,22 @@ class TestCostVolume(unittest.TestCase):
         image_shape = (2, 2, 2, 1)
         c1 = [[1, 2], [2, 3]]
         c2 = [[1, 3], [4, 5]]
-        c1 = np.expand_dims(np.repeat(np.expand_dims(c1, axis=0), 2, axis=0), axis=-1)
-        c2 = np.expand_dims(np.repeat(np.expand_dims(c2, axis=0), 2, axis=0), axis=-1)
+        c1 = np.expand_dims(c1, axis=-1)
+        c2 = np.expand_dims(c2, axis=-1)
+
+        ones_shape = (2, 2, 1)
+        ones1 = np.ones(ones_shape)
+        ones2 = np.ones(ones_shape)
+
+        c1 = np.concatenate([c1, ones1])
+        c2 = np.concatenate([c2, ones2])
+
         expected = np.array([
             [[0, 0, 0, 0, 1, 3, 0, 4, 5], [0, 0, 0, 2, 6, 0, 8, 10, 0]],
             [[0, 2, 6, 0, 8, 10, 0, 0, 0], [3, 9, 0, 12, 15, 0, 0, 0, 0]]
         ]).astype(np.float32)
-        expected = np.expand_dims(expected, axis=0)
-        expected = np.repeat(expected, 2, axis=0)
+        expected_ones = np.ones(np.shape(expected))
+        expected = np.concatenate([expected, expected_ones])
         input1 = tf.placeholder(shape=image_shape, dtype=tf.float32)
         input2 = tf.placeholder(shape=image_shape, dtype=tf.float32)
         cv = cost_volume(input1, input2, 1)
@@ -105,83 +113,6 @@ class TestCostVolume(unittest.TestCase):
         cv = cost_volume(input1, input2, 1)
         cv = self.sess.run(cv, feed_dict={input1: c1, input2: c2})
         self.assertEqual(np.squeeze(cv).tolist(), expected.tolist())
-
-    """
-    def testSearch0SmallImage(self):
-        image_shape = (1, 2, 2, 2)
-        c1 = [
-            [[1, 3], [2, 4]],
-            [[2, 6], [3, 10]]
-        ]
-        c2 = [
-            [[1, 3], [3, 5]],
-            [[4, 4], [5, 5]]
-        ]
-        c1 = np.expand_dims(c1, axis=0)
-        c2 = np.expand_dims(c2, axis=0)
-        expected = np.array([
-            [[0, 0, 0, 0, 1, 3, 0, 4, 5], [0, 0, 0, 2, 6, 0, 8, 10, 0]],
-            [[0, 2, 6, 0, 8, 10, 0, 0, 0], [3, 9, 0, 12, 15, 0, 0, 0, 0]]
-        ]).astype(np.float32)
-        input1 = tf.placeholder(shape=image_shape, dtype=tf.float32)
-        input2 = tf.placeholder(shape=image_shape, dtype=tf.float32)
-        cv = cost_volume(input1, input2, 1)
-        cv = self.sess.run(cv, feed_dict={input1: c1, input2: c2})
-        self.assertEqual(np.squeeze(cv).tolist(), expected.tolist())
-    
-
-    def testSearch1SmallImage(self):
-"""
-#
-#
-# class TestSpacialTransformTranslate(unittest.TestCase):
-#     def setUp(self):
-#         # Image dimensions.
-#         self.height = 70
-#         self.width = 90
-#         self.channels = 3
-#         self.image_shape = (1, self.height, self.width, self.channels)
-#
-#         # Defines how many pixels to translate by.
-#         trans_amount = 20
-#
-#         # Defines the square position and size.
-#         y_start = 10
-#         y_end = 40
-#         x_start = 20
-#         x_end = 50
-#
-#         # The maximum diff is the circumference of the square.
-#         self.max_diff = (x_end - x_start) * 2 + (y_end - y_start) * 2
-#
-#         # Input.
-#         self.box_image = np.zeros(shape=self.image_shape, dtype=np.float)
-#         self.box_image[:, y_start:y_end, x_start:x_end, :] = 1.0
-#
-#         # Expected output.
-#         self.translated_box_image = np.zeros(shape=self.image_shape, dtype=np.float)
-#         self.translated_box_image[:, y_start+trans_amount:y_end+trans_amount,
-#                                   x_start+trans_amount:x_end+trans_amount, :] = 1.0
-#
-#         # Warp matrix to achieve the expected output.
-#         self.warp = np.asarray([[1.0, 0.0, -float(trans_amount) / self.width * 2.0,
-#                                  0.0, 1.0, -float(trans_amount) / self.height * 2.0]], dtype=np.float)
-#
-#         self.sess = tf.Session()
-#
-#     def runTest(self):
-#         input = tf.placeholder(shape=self.image_shape, dtype=tf.float32)
-#         theta = tf.placeholder(shape=[self.image_shape[0], 6], dtype=tf.float32)
-#         transformed = spatial_transformer_network(input, theta)
-#
-#         # Get the resulting image.
-#         transformed_image = self.sess.run(transformed, feed_dict={input: self.box_image, theta: self.warp})
-#
-#         # Get the diff.
-#         diff_img = np.abs(transformed_image - self.translated_box_image)
-#         diff = np.sum(np.mean(diff_img, axis=-1))
-#
-#         self.assertLess(diff, self.max_diff)
 
 
 if __name__ == '__main__':
