ca96c912630d422788633bcb6fb62ad83dd724e0
TianXing Li <tianxingli24@gmail.com>
2018-05-12 18:01:38 -0400

    Added down-sampling and up-sampling blocks.

--- gridnet/modules.py
+++ gridnet/modules.py
@@ -2,7 +2,54 @@ import tensorflow as tf
 from common.models import ConvNetwork
 
 
+def get_dropped_out_one(dropout_rate):
+    one = tf.constant(1.0)
+    return tf.layers.dropout(one, rate=dropout_rate)
+
+
 class LateralBlock(ConvNetwork):
+    def __init__(self, name, layer_specs,
+                 activation_fn=tf.nn.leaky_relu,
+                 use_batch_norm=False,
+                 total_dropout_rate=0.0,
+                 regularizer=None):
+        """
+        :param name: Str. For variable scoping.
+        :param layer_specs: Array of shape [num_layers, 2]. Constrained version of parent class' layer_specs.
+                            The second dimension consists of [num_output_features, dilation].
+        :param activation_fn: Tensorflow activation function. This will not be applied on the last convolutional layer.
+        :param use_batch_norm: Whether to use batch normalization.
+        :param total_dropout_rate: A value of 1.0 will always zero-out the output of this block, and 0.0 will keep it.
+        :param regularizer: Tf regularizer such as tf.contrib.layers.l2_regularizer.
+        """
+        full_layer_specs = []
+        for i, layer_spec in enumerate(layer_specs):
+            full_layer_spec = [3, layer_spec[0], layer_spec[1], 1]
+            full_layer_specs.append(full_layer_spec)
+
+        super().__init__(layer_specs=full_layer_specs,
+                         activation_fn=activation_fn, last_activation_fn=None,
+                         regularizer=regularizer, padding='SAME')
+
+        self.name = name
+        self.total_dropout_rate = total_dropout_rate
+
+    def get_forward(self, features, reuse_variables=False):
+        """
+        :param features: Tensor. Feature map of shape [batch_size, H, W, num_features].
+        :param reuse_variables: Bool. Whether to reuse the variables.
+        :return: Tensor. Feature map of shape [batch_size, H, W, num_features].
+        """
+        with tf.variable_scope(self.name, reuse=reuse_variables):
+
+            # Pass through resolution preserving convolutions, with skip connection at the end.
+            previous_output = self.activation_fn(features)
+            previous_output, layer_outputs = self._get_conv_tower(previous_output)
+            dropped_out = previous_output * get_dropped_out_one(self.total_dropout_rate)
+            return dropped_out + features
+
+
+class DownSamplingBlock(ConvNetwork):
     def __init__(self, name, layer_specs,
                  activation_fn=tf.nn.leaky_relu,
                  use_batch_norm=False,
@@ -29,6 +76,7 @@ class LateralBlock(ConvNetwork):
                          regularizer=regularizer, padding='SAME')
 
         self.name = name
+        self.total_dropout_rate = total_dropout_rate
 
     def get_forward(self, features, reuse_variables=False):
         """
@@ -39,9 +87,52 @@ class LateralBlock(ConvNetwork):
         with tf.variable_scope(self.name, reuse=reuse_variables):
             previous_output = self.activation_fn(features)
             final_output, layer_outputs = self._get_conv_tower(previous_output)
-            return final_output
+            return final_output * get_dropped_out_one(self.total_dropout_rate)
 
-# Down-sampling block.
 
-# Up-sampling block.
+class UpSamplingBlock(ConvNetwork):
+    def __init__(self, name, layer_specs,
+                 activation_fn=tf.nn.leaky_relu,
+                 use_batch_norm=False,
+                 total_dropout_rate=0.0,
+                 regularizer=None):
+        """
+        :param name: Str. For variable scoping.
+        :param layer_specs: Array of shape [num_layers, 2]. Constrained version of parent class' layer_specs.
+                            The second dimension consists of [num_output_features, dilation].
+        :param activation_fn: Tensorflow activation function. This will not be applied on the last convolutional layer.
+        :param use_batch_norm: Whether to use batch normalization.
+        :param total_dropout_rate: A value of 1.0 will always zero-out the output of this block, and 0.0 will keep it.
+        :param regularizer: Tf regularizer such as tf.contrib.layers.l2_regularizer.
+        """
+        full_layer_specs = []
+        for i, layer_spec in enumerate(layer_specs):
+            full_layer_spec = [3, layer_spec[0], layer_spec[1], 1]
+            full_layer_specs.append(full_layer_spec)
+
+        super().__init__(layer_specs=full_layer_specs,
+                         activation_fn=activation_fn, last_activation_fn=None,
+                         regularizer=regularizer, padding='SAME')
+
+        self.name = name
+        self.total_dropout_rate = total_dropout_rate
+
+    def get_forward(self, features, reuse_variables=False):
+        """
+        :param features: Tensor. Feature map of shape [batch_size, H, W, num_features].
+        :param reuse_variables: Bool. Whether to reuse the variables.
+        :return: Tensor. Feature map of shape [batch_size, H, W, num_features].
+        """
+        with tf.variable_scope(self.name, reuse=reuse_variables):
+
+            # Up-sample feature images.
+            cur_height, cur_width = tf.shape(features)[1], tf.shape(features)[2]
+            new_height, new_width = 2 * cur_height, 2 * cur_width
+            previous_output = tf.image.resize_bilinear(features, (new_height, new_width))
+
+            # Pass through resolution preserving convolutions.
+            previous_output = self.activation_fn(previous_output)
+            previous_output, layer_outputs = self._get_conv_tower(previous_output)
+            dropped_out = previous_output * get_dropped_out_one(self.total_dropout_rate)
+            return dropped_out
 
