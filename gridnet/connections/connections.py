Fixed total dropout scaling, no modifications are made in testing mode anymore.

gridnet/connections/connections.py
@@ -40,15 +40,16 @@ class LateralConnection(ConvNetwork):
             # Pass through resolution preserving convolutions, with skip connection at the end.
             previous_output = self.activation_fn(features)
             previous_output, layer_outputs = self._get_conv_tower(previous_output)
-            previous_output += features
+            final_output = features + previous_output
 
             # Total dropout.
             if training:
-                dropped_out = previous_output * tf.cast(tf_coin_flip(1.0 - self.total_dropout_rate), tf.float32)
-            else:
-                dropped_out = previous_output / (1.0 - self.total_dropout_rate)
+                epsilon = 1E-6
+                is_heads = tf_coin_flip(1.0 - self.total_dropout_rate)
+                factor = tf.cast(is_heads, tf.float32) / (1.0 - self.total_dropout_rate + epsilon)
+                final_output *= factor
 
-            return dropped_out
+            return final_output
 
 
 class DownSamplingConnection(ConvNetwork):
@@ -113,7 +114,7 @@ class UpSamplingConnection(ConvNetwork):
 
         self.name = name
 
-    def get_forward(self, features, reuse_variables=False, training=False):
+    def get_forward(self, features, reuse_variables=False):
         """
         :param features: Tensor. Feature map of shape [batch_size, H, W, num_features].
         :param reuse_variables: Bool. Whether to reuse the variables.

gridnet/connections/connections_test.py
@@ -135,7 +135,7 @@ class TestConnections(unittest.TestCase):
         connection = UpSamplingConnection(name, specs,
                                           regularizer=l2_regularizer(1e-4))
         input_features_tensor = tf.placeholder(shape=[None, height, width, num_features], dtype=tf.float32)
-        output = connection.get_forward(input_features_tensor, training=True)
+        output = connection.get_forward(input_features_tensor)
 
         input_features = np.zeros(shape=[batch_size, height, width, num_features], dtype=np.float32)
         input_features[:, 0:height, 0:width, :] = 1.0

gridnet/gridnet.py
@@ -52,7 +52,7 @@ class GridNet:
         # More settings
         self.activation_fn = tf.keras.layers.PReLU
 
-        # Construct specs for lateral connections.
+        # Construct specs for connections.
         # Entry specs[i][j] is the spec for the jth convolution for any connection in the ith row.
         self.lateral_specs = []
         self.upsample_specs = []
@@ -87,7 +87,7 @@ class GridNet:
             # First lateral connection.
             grid_outputs[0][0] = self.process_rightwards(features, 0, 0)
 
-            # Connect first half by iterating to the right, and downwards.
+            # Connect first half (Down-sampling streams) by iterating to the right, and downwards.
             for i in range(self.height):
                 for j in range(self.width / 2):
                     if i == 0 and j == 0:
@@ -101,7 +101,7 @@ class GridNet:
 
                     grid_outputs[i][j] = top_output + left_output
 
-            # Connect second half by iterating to the right, and upwards.
+            # Connect second half (Up-sampling streams) by iterating to the right, and upwards.
             for i in range(self.height - 1, 0, -1):
                 for j in range(self.width / 2, self.width):
                     bottom_output, left_output = 0, 0
@@ -112,6 +112,7 @@ class GridNet:
 
                     grid_outputs[i][j] = bottom_output + left_output
 
+            # Final lateral connection.
             previous_output = grid_outputs[0][self.width-1]
             final_output = self.process_rightwards(previous_output, 0, self.width)
             return final_output, grid_outputs