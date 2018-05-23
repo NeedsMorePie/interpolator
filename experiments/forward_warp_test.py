17ec7d9ed776c36e00f070e63c7ccf1dc681663f
TianXing Li <tianxingli24@gmail.com>
2018-05-22 22:08:20 -0400

    Fixed the inverse bilinear interpolation.

--- experiments/forward_warp_test.py
+++ experiments/forward_warp_test.py
@@ -2,10 +2,12 @@ import unittest
 import os
 from utils.flow import read_flow_file
 from utils.img import read_image, show_image
-from forward_warp.warp import forward_warp
+from experiments.forward_warp import forward_warp
+import matplotlib.image as mpimg
 import numpy as np
+import cv2
 
-VISUALIZE = False
+VISUALIZE = True
 
 
 class TestForwardWarp(unittest.TestCase):
@@ -18,4 +20,21 @@ class TestForwardWarp(unittest.TestCase):
         flow_ab = read_flow_file(root_dir + 'pwcnet/warp/test_data/flow_ab.flo')
         img_a = read_image(root_dir + 'pwcnet/warp/test_data/image_a.png', as_float=True)
         warped = forward_warp(img_a, flow_ab, 1.0)
+        warped = np.clip(warped, 0.0, 1.0)
         show_image(warped)
+
+        # For writing to video.
+        # height = img_a.shape[0]
+        # width = img_a.shape[1]
+        # writer = cv2.VideoWriter(cur_dir + '/outputs/video.avi', cv2.VideoWriter_fourcc(*"MJPG"), 20, (width, height))
+        #
+        # steps = 20
+        # for i in range(steps):
+        #     t = i * (1.0 / float(steps))
+        #     warped = forward_warp(img_a, flow_ab, t)
+        #     warped = np.clip(warped, 0.0, 1.0)
+        #     output_path = cur_dir + "/outputs/out-%.2f.png" % t
+        #     mpimg.imsave(output_path, warped)
+        #     writer.write(cv2.imread(output_path))
+        #
+        # writer.release()
