45a92ed4f47ab2b64807bf38986a5e864c13608e
TianXing Li <tianxingli24@gmail.com>
2018-05-26 23:26:43 -0400

    Added visualization and cleaned up test.

--- data/interp/interp_data_test.py
+++ data/interp/interp_data_test.py
@@ -6,6 +6,7 @@ import unittest
 from utils.img import show_image
 from data.interp.interp_data import InterpDataSet
 
+VISUALIZE = False
 
 class TestInterpDataSet(unittest.TestCase):
     def setUp(self):
@@ -56,44 +57,18 @@ class TestInterpDataSet(unittest.TestCase):
         self.data_set.load(self.sess)
 
         next_sequence_tensor = self.data_set.get_next_batch()
+
+        # There are 6 valid sequences in total, and we are using a batch size of 2.
         for i in range(3):
             next_sequence = self.sess.run(next_sequence_tensor, feed_dict=self.data_set.get_train_feed_dict())
             self.assertTupleEqual(np.shape(next_sequence), (2, 3, 264, 470, 3))
-
-        # images_1_a, images_1_b, flows = self.sess.run([next_images_a, next_images_b, next_flows],
-        #                                               feed_dict=self.data_set.get_train_feed_dict())
-        # self.assertTupleEqual(images_1_a.shape, (2, 436, 1024, 3))
-        # self.assertTupleEqual(images_1_b.shape, (2, 436, 1024, 3))
-        # self.assertTupleEqual(flows.shape, (2, 436, 1024, 2))
-        # images_2_a, images_2_b, flows = self.sess.run([next_images_a, next_images_b, next_flows],
-        #                                               feed_dict=self.data_set.get_train_feed_dict())
-        # self.assertTupleEqual(images_2_a.shape, (2, 436, 1024, 3))
-        # self.assertTupleEqual(images_2_b.shape, (2, 436, 1024, 3))
-        # self.assertTupleEqual(flows.shape, (2, 436, 1024, 2))
-        #
-        # # Make sure all the images are different (i.e. read correctly).
-        # self.assertFalse(np.allclose(images_1_a[0], images_1_a[1]))
-        # self.assertFalse(np.allclose(images_2_a[0], images_1_a[1]))
-        # self.assertFalse(np.allclose(images_1_a[0], images_2_a[1]))
-        # self.assertFalse(np.allclose(images_2_a[0], images_2_a[1]))
-        # self.assertTrue(np.max(images_1_a[0]) <= 1.0)
-        #
-        # # Validation data size is 1, so even though the dataset batch size is 2, the validation batch size is 1.
-        # self.data_set.init_validation_data(self.sess)
-        # images_a, images_b, flows = self.sess.run([next_images_a, next_images_b, next_flows],
-        #                                           feed_dict=self.data_set.get_validation_feed_dict())
-        # self.assertTupleEqual(images_a.shape, (1, 436, 1024, 3))
-        # self.assertTupleEqual(images_b.shape, (1, 436, 1024, 3))
-        # self.assertTupleEqual(flows.shape, (1, 436, 1024, 2))
-        # # If init_validation_data isn't called again, validation dataset has hit the end of its epoch and will be out
-        # # of range.
-        # end_of_dataset = False
-        # try:
-        #     self.sess.run([next_images_a, next_images_b, next_flows],
-        #                   feed_dict=self.data_set.get_validation_feed_dict())
-        # except tf.errors.OutOfRangeError:
-        #     end_of_dataset = True
-        # self.assertTrue(end_of_dataset)
+            if VISUALIZE:
+                for j in range(3):
+                    print('Showing sequence ...')
+                    show_image(next_sequence[0][j])
+                for j in range(3):
+                    print('Showing sequence ...')
+                    show_image(next_sequence[1][j])
 
     def tearDown(self):
         output_paths = self.data_set.get_train_file_names() + self.data_set.get_validation_file_names()
