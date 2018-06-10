Removed pixel translation tests. Replaced map_fn usage with scatter_nd with batch indices. Changed warp implementation to be simpler, faster, and have incorrect sub-gradients for (very rare) corner cases. Simplified and relaxed constraints on flow gradient tests accordingly.

forward_warp/forward_warp.py
@@ -25,25 +25,19 @@ def forward_warp(features, flow):
     within_bounds = greater_than_zero * less_than_height * less_than_width
     splat_values *= tf.expand_dims(within_bounds, axis=-1)
 
-    # Clip indices and flatten.
+    # Add batch indices, clip xy indices, and flatten.
+    batch_size = tf.shape(features)[0]
+    num_indices = tf.shape(indices)[1]
+    batch_indices = tf.range(0, batch_size)
+    batch_indices = tf.tile(batch_indices, [num_indices])
+    batch_indices = tf.reshape(batch_indices, (num_indices, batch_size))
+    batch_indices = tf.transpose(batch_indices)
     y_indices = tf.clip_by_value(indices[..., 0], 0, height-1)
     x_indices = tf.clip_by_value(indices[..., 1], 0, width-1)
-    indices = tf.stack([y_indices, x_indices], axis=-1)
+    indices = tf.stack([batch_indices, y_indices, x_indices], axis=-1)
 
     # Scatter into output.
-    all_warped = []
-
-    def _scatter(elms):
-        splat_values, indices, features = elms
-        warped = tf.scatter_nd(indices, splat_values, tf.shape(features))
-        all_warped.append(warped)
-        summed = tf.add_n(all_warped)
-
-        # map_fn requires us to return the same number of things as arguments (nested structure must match).
-        # See: https://stackoverflow.com/questions/47984876/tensorflow-tf-map-fn-parameters
-        return summed, summed, summed
-
-    warped, _, _ = tf.map_fn(_scatter, (splat_values, indices, features), back_prop=True)
+    warped = tf.scatter_nd(indices, splat_values, tf.shape(features))
     return warped
 
 
@@ -64,25 +58,12 @@ def get_translated_pixels(features, translations):
     translated_indices = translations + indices_2d
 
     # Get splat corners.
-    ceiled = tf.cast(tf.ceil(translated_indices), tf.int32)
     floored = tf.cast(tf.floor(translated_indices), tf.int32)
+    ceiled = floored + 1
     tl_indices = floored
     tr_indices = tf.stack([floored[..., 0], ceiled[..., 1]], axis=-1)
     br_indices = ceiled
     bl_indices = tf.stack([ceiled[..., 0], floored[..., 1]], axis=-1)
-
-    # If both x and y have integer coordinates (ceil and floor are the same),
-    # shift top-left to top, top-right to right, bottom-right to bottom, and bottom-left to left.
-    x_shift = tf.stack([tf.zeros(tf.shape(x_2d)), tf.ones(tf.shape(x_2d))], axis=-1)
-    y_shift = tf.stack([tf.ones(tf.shape(x_2d)), tf.zeros(tf.shape(x_2d))], axis=-1)
-    ceiled_compare = tf.cast(ceiled, tf.float32)
-    is_duplicated = tf.cast(tf.equal(ceiled_compare, translated_indices), tf.float32)
-    are_both_duplicated = tf.expand_dims(is_duplicated[..., 0] * is_duplicated[..., 1], axis=-1)
-    center_indices = tl_indices
-    tl_indices -= tf.cast(y_shift * are_both_duplicated, tf.int32)
-    tr_indices += tf.cast(x_shift * are_both_duplicated, tf.int32)
-    bl_indices += tf.cast(y_shift * are_both_duplicated, tf.int32)
-    br_indices -= tf.cast(x_shift * are_both_duplicated, tf.int32)
 
     # Compute splat values, using inverse bi-linear interpolation formula.
     tl_diff = tf.expand_dims(tf.abs(tf.cast(tl_indices, tf.float32) - translated_indices), axis=-1)
@@ -93,18 +74,10 @@ def get_translated_pixels(features, translations):
     tr_vals = features * (1.0 - tr_diff[..., 0, :]) * (1.0 - tr_diff[..., 1, :])
     br_vals = features * (1.0 - br_diff[..., 0, :]) * (1.0 - br_diff[..., 1, :])
     bl_vals = features * (1.0 - bl_diff[..., 0, :]) * (1.0 - bl_diff[..., 1, :])
-    center_vals = features
-
-    # Zero out certain splat values if x and y have integer coordinates (ceil and floor are the same).
-    # Otherwise we get incorrect duplication.
-    center_vals *= are_both_duplicated
-    tr_vals *= 1.0 - tf.expand_dims(is_duplicated[..., 1], axis=-1) * (1.0 - are_both_duplicated)
-    br_vals *= 1.0 - tf.expand_dims(is_duplicated[..., 0], axis=-1) * (1.0 - are_both_duplicated)
-    bl_vals *= 1.0 - tf.expand_dims(tf.reduce_sum(is_duplicated, axis=-1), axis=-1) * (1.0 - are_both_duplicated)
 
     # Combine and flatten shape.
-    all_indices = tf.stack([tl_indices, tr_indices, br_indices, bl_indices, center_indices], axis=1)
-    all_vals = tf.stack([tl_vals, tr_vals, br_vals, bl_vals, center_vals], axis=1)
+    all_indices = tf.stack([tl_indices, tr_indices, br_indices, bl_indices], axis=1)
+    all_vals = tf.stack([tl_vals, tr_vals, br_vals, bl_vals], axis=1)
     all_indices = tf.reshape(all_indices, (batch_size, -1, 2))
     all_vals = tf.reshape(all_vals, (batch_size, -1, channels))
     return all_indices, all_vals

forward_warp/forward_warp_test.py
@@ -15,275 +15,6 @@ class TestForwardWarp(unittest.TestCase):
         config.gpu_options.allow_growth = True
         self.sess = tf.Session(config=config)
 
-    def test_push_pixels_whole_1(self):
-        height = 2
-        width = 2
-        features = [[
-            [[1, 0], [0, 0]],
-            [[0, 1], [0, 0]]
-        ]]
-
-        # Translation is in (y, x) order.
-        # Translates first column to second column.
-        translations = [[
-            [[0, 1], [0, 0]],
-            [[0, 1], [0, 0]]
-        ]]
-
-        # Indices are in (y, x) order.
-        expected_indices = [[
-            [0, 1], [-1, 1], [1, 1], [0, 0], [0, 2],
-            [0, 1], [-1, 1], [1, 1], [0, 0], [0, 2],
-            [1, 1], [0, 1], [2, 1], [1, 0], [1, 2],
-            [1, 1], [0, 1], [2, 1], [1, 0], [1, 2]
-        ]]
-
-        expected_values = [[
-            [1, 0], [0, 0], [0, 0], [0, 0], [0, 0],
-            [0, 0], [0, 0], [0, 0], [0, 0], [0, 0],
-            [0, 1], [0, 0], [0, 0], [0, 0], [0, 0],
-            [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]
-        ]]
-
-        expected_indices, expected_values = np.squeeze(expected_indices), np.squeeze(expected_values)
-        expected = np.stack([expected_indices, expected_values], axis=-2).tolist()
-
-        features_tensor = tf.placeholder(tf.float32, shape=[1, height, width, 2])
-        translations_tensor = tf.placeholder(tf.float32, shape=[1, height, width, 2])
-        indices_tensor, values_tensor = get_translated_pixels(features_tensor, translations_tensor)
-
-        query = [indices_tensor, values_tensor]
-        indices, values = self.sess.run(query, feed_dict={features_tensor: features, 
-                                                          translations_tensor: translations})
-
-        indices, values = indices.tolist(), values.tolist()
-        indices, values = np.squeeze(indices), np.squeeze(values)
-        predicted = np.stack([indices, values], axis=-2).tolist()
-        self.assertCountEqual(predicted, expected)
-
-    def test_push_pixels_whole_2(self):
-        height = 2
-        width = 3
-        features = [[
-            [[1, 0], [0, 0], [-1, 0]],
-            [[0, 1], [0, 0], [-1, 0]]
-        ]]
-
-        # Translation is in (y, x) order.
-        # Translates first column to second column.
-        translations = [[
-            [[0, 1], [0, 0], [1, 0]],
-            [[0, 1], [0, 0], [0, 0]]
-        ]]
-
-        # Indices are in (y, x) order.
-        expected_indices = [[
-            [0, 1], [-1, 1], [1, 1], [0, 0], [0, 2],
-            [0, 1], [-1, 1], [1, 1], [0, 0], [0, 2],
-            [1, 2], [0, 2], [2, 2], [1, 1], [1, 3],
-            [1, 1], [0, 1], [2, 1], [1, 0], [1, 2],
-            [1, 1], [0, 1], [2, 1], [1, 0], [1, 2],
-            [1, 2], [0, 2], [2, 2], [1, 1], [1, 3]
-        ]]
-
-        # We expect the duplicates to not splat anything.
-        expected_values = [[
-            [1, 0], [0, 0], [0, 0], [0, 0], [0, 0],
-            [0, 0], [0, 0], [0, 0], [0, 0], [0, 0],
-            [-1, 0], [0, 0], [0, 0], [0, 0], [0, 0],
-            [0, 1], [0, 0], [0, 0], [0, 0], [0, 0],
-            [0, 0], [0, 0], [0, 0], [0, 0], [0, 0],
-            [-1, 0], [0, 0], [0, 0], [0, 0], [0, 0]
-        ]]
-
-        expected_indices, expected_values = np.squeeze(expected_indices), np.squeeze(expected_values)
-        expected = np.stack([expected_indices, expected_values], axis=-2).tolist()
-
-        features_tensor = tf.placeholder(tf.float32, shape=[1, height, width, 2])
-        translations_tensor = tf.placeholder(tf.float32, shape=[1, height, width, 2])
-        indices_tensor, values_tensor = get_translated_pixels(features_tensor, translations_tensor)
-
-        query = [indices_tensor, values_tensor]
-        indices, values = self.sess.run(query, feed_dict={features_tensor: features,
-                                                          translations_tensor: translations})
-
-        indices, values = indices.tolist(), values.tolist()
-        indices, values = np.squeeze(indices), np.squeeze(values)
-        predicted = np.stack([indices, values], axis=-2).tolist()
-        self.assertCountEqual(predicted, expected)
-
-    def test_push_pixels_partial_1(self):
-        """
-        X or y coordinate are integers, but only 1 of them at a time.
-        """
-        height = 2
-        width = 2
-        features = [[
-            [[1, 0], [0, 0]],
-            [[0, 1], [0, 0]]
-        ]]
-
-        # Translation is in (y, x) order.
-        translations = [[
-            [[0.5, 0], [0, 0]],
-            [[0, 0.5], [0, 0]]
-        ]]
-
-        # Indices are in (y, x) order.
-        expected_indices = [[
-            [0, 0], [0, 0], [1, 0], [1, 0], [0, 0],
-            [0, 1], [-1, 1], [1, 1], [0, 0], [0, 2],
-            [1, 0], [1, 0], [1, 1], [1, 1], [1, 0],
-            [1, 1], [0, 1], [2, 1], [1, 0], [1, 2]
-        ]]
-
-        # We expect the duplicates to not splat anything.
-        expected_values = [[
-            [0.5, 0], [0, 0], [0.5, 0], [0, 0], [0, 0],
-            [0, 0], [0, 0], [0, 0], [0, 0], [0, 0],
-            [0, 0.5], [0, 0], [0, 0.5], [0, 0], [0, 0],
-            [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]
-        ]]
-
-        expected_indices, expected_values = np.squeeze(expected_indices), np.squeeze(expected_values)
-        expected = np.stack([expected_indices, expected_values], axis=-2).tolist()
-
-        features_tensor = tf.placeholder(tf.float32, shape=[1, height, width, 2])
-        translations_tensor = tf.placeholder(tf.float32, shape=[1, height, width, 2])
-        indices_tensor, values_tensor = get_translated_pixels(features_tensor, translations_tensor)
-
-        query = [indices_tensor, values_tensor]
-        indices, values = self.sess.run(query, feed_dict={features_tensor: features,
-                                                          translations_tensor: translations})
-
-        indices, values = indices.tolist(), values.tolist()
-        indices, values = np.squeeze(indices), np.squeeze(values)
-        predicted = np.stack([indices, values], axis=-2).tolist()
-        self.assertCountEqual(predicted, expected)
-
-    def test_push_pixels_partial_2(self):
-        height = 2
-        width = 2
-        features = [[
-            [[1, 0], [0, 0]],
-            [[0, 1], [0, 0]]
-        ]]
-
-        # Translation is in (y, x) order.
-        translations = [[
-            [[0.5, 0.5], [0, 0]],
-            [[0, -1], [0, 0]]
-        ]]
-
-        # Indices are in (y, x) order.
-        expected_indices = [[
-            [0, 0], [0, 1], [1, 0], [1, 1], [0, 0],
-            [0, 1], [-1, 1], [1, 1], [0, 0], [0, 2],
-            [1, -1], [0, -1], [2, -1], [1, -2], [1, 0],
-            [1, 1], [0, 1], [2, 1], [1, 0], [1, 2]
-        ]]
-
-        # We expect the duplicates to not splat anything.
-        expected_values = [[
-            [0.25, 0], [0.25, 0], [0.25, 0], [0.25, 0], [0, 0],
-            [0, 0], [0, 0], [0, 0], [0, 0], [0, 0],
-            [0, 1], [0, 0], [0, 0], [0, 0], [0, 0],
-            [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]
-        ]]
-
-        expected_indices, expected_values = np.squeeze(expected_indices), np.squeeze(expected_values)
-        expected = np.stack([expected_indices, expected_values], axis=-2).tolist()
-
-        features_tensor = tf.placeholder(tf.float32, shape=[1, height, width, 2])
-        translations_tensor = tf.placeholder(tf.float32, shape=[1, height, width, 2])
-        indices_tensor, values_tensor = get_translated_pixels(features_tensor, translations_tensor)
-
-        query = [indices_tensor, values_tensor]
-        indices, values = self.sess.run(query, feed_dict={features_tensor: features,
-                                                          translations_tensor: translations})
-
-        indices, values = indices.tolist(), values.tolist()
-        indices, values = np.squeeze(indices), np.squeeze(values)
-        predicted = np.stack([indices, values], axis=-2).tolist()
-        self.assertCountEqual(predicted, expected)
-
-    def test_push_pixels_batch(self):
-        height = 2
-        width = 2
-        features = [
-            [
-                [[1, 0], [0, 0]],
-                [[0, 1], [0, 0]]
-            ],
-            [
-                [[1, 0], [0, 0]],
-                [[0, 1], [0, 0]]
-            ]
-        ]
-
-        # Translation is in (y, x) order.
-        # Translates first column to second column.
-        translations = [
-            [
-                [[0, 1], [0, 0]],
-                [[0, 1], [0, 0]]
-            ],
-            [
-                [[0.5, 0], [0, 0]],
-                [[0, 0.5], [0, 0]]
-            ]
-        ]
-
-        # Indices are in (y, x) order.
-        # As the pixels are splatted on at exact integer coordinates, there will be duplicates.
-        expected_indices = [
-            [
-                [0, 1], [-1, 1], [1, 1], [0, 0], [0, 2],
-                [0, 1], [-1, 1], [1, 1], [0, 0], [0, 2],
-                [1, 1], [0, 1], [2, 1], [1, 0], [1, 2],
-                [1, 1], [0, 1], [2, 1], [1, 0], [1, 2]
-            ],
-            [
-                [0, 0], [0, 0], [1, 0], [1, 0], [0, 0],
-                [0, 1], [-1, 1], [1, 1], [0, 0], [0, 2],
-                [1, 0], [1, 0], [1, 1], [1, 1], [1, 0],
-                [1, 1], [0, 1], [2, 1], [1, 0], [1, 2]
-            ]
-        ]
-
-        # We expect the duplicates to not splat anything.
-        expected_values = [
-            [
-                [1, 0], [0, 0], [0, 0], [0, 0], [0, 0],
-                [0, 0], [0, 0], [0, 0], [0, 0], [0, 0],
-                [0, 1], [0, 0], [0, 0], [0, 0], [0, 0],
-                [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]
-            ],
-            [
-                [0.5, 0], [0, 0], [0.5, 0], [0, 0], [0, 0],
-                [0, 0], [0, 0], [0, 0], [0, 0], [0, 0],
-                [0, 0.5], [0, 0], [0, 0.5], [0, 0], [0, 0],
-                [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]
-            ]
-        ]
-
-        expected_indices, expected_values = np.squeeze(expected_indices), np.squeeze(expected_values)
-        expected = np.stack([expected_indices, expected_values], axis=-2).tolist()
-
-        features_tensor = tf.placeholder(tf.float32, shape=[2, height, width, 2])
-        translations_tensor = tf.placeholder(tf.float32, shape=[2, height, width, 2])
-        indices_tensor, values_tensor = get_translated_pixels(features_tensor, translations_tensor)
-
-        query = [indices_tensor, values_tensor]
-        indices, values = self.sess.run(query, feed_dict={features_tensor: features,
-                                                          translations_tensor: translations})
-
-        indices, values = indices.tolist(), values.tolist()
-        indices, values = np.squeeze(indices), np.squeeze(values)
-        predicted = np.stack([indices, values], axis=-2).tolist()
-        self.assertCountEqual(predicted[0], expected[0])
-        self.assertCountEqual(predicted[1], expected[1])
-
     def test_forward_warp_whole_1(self):
         height = 2
         width = 2
@@ -367,16 +98,10 @@ class TestForwardWarp(unittest.TestCase):
         for grad_tensor in grads_tensor:
             self.assertNotEqual(grad_tensor, None)
 
-        # For the top left warp, there should only be 2 flows giving non-zero gradients.
         grads = self.sess.run(grads_tensor, feed_dict={flow_tensor: flow, features_tensor: features})
         flow_grads, feature_grads = grads[0][0], grads[1][0]
-        self.assertNotEqual(np.sum(flow_grads[0][0]), 0.0)
-        self.assertNotEqual(np.sum(flow_grads[1][0]), 0.0)
-        self.assertEqual(np.sum(flow_grads) - np.sum(flow_grads[0][0]) - np.sum(flow_grads[1][0]), 0.0)
-
-        # Note that the feature on the left edge (at [1][0]) should have 0 gradient.
-        self.assertNotEqual(np.sum(feature_grads[0][0]), 0.0)
-        self.assertEqual(np.sum(feature_grads) - np.sum(feature_grads[0][0]), 0.0)
+        self.assertNotEqual(np.sum(flow_grads), 0.0)
+        self.assertNotEqual(np.sum(feature_grads), 0.0)
 
     def test_forward_warp_oob(self):
         """
@@ -453,6 +178,16 @@ class TestForwardWarp(unittest.TestCase):
         self.assertEqual(warp[0].tolist(), expected_warp[0])
         self.assertEqual(warp[1].tolist(), expected_warp[1])
 
+        # Check for gradients.
+        grads_tensor = tf.gradients(warp_tensor[0][0][0], [flow_tensor, features_tensor])
+        for grad_tensor in grads_tensor:
+            self.assertNotEqual(grad_tensor, None)
+
+        grads = self.sess.run(grads_tensor, feed_dict={flow_tensor: flow, features_tensor: features})
+        flow_grads, feature_grads = grads[0][0], grads[1][0]
+        self.assertNotEqual(np.sum(flow_grads), 0.0)
+        self.assertNotEqual(np.sum(feature_grads), 0.0)
+
     def test_visualization(self):
         if not VISUALIZE:
             return
@@ -476,7 +211,7 @@ class TestForwardWarp(unittest.TestCase):
         # For writing to video.
         # height = img_a[0].shape[0]
         # width = img_a[0].shape[1]
-        # writer = cv2.VideoWriter(cur_dir + '/outputs/video-jank.avi', cv2.VideoWriter_fourcc(*"MJPG"), 20, (width, height))
+        # writer = cv2.VideoWriter(cur_dir + '/outputs/warped.avi', cv2.VideoWriter_fourcc(*"MJPG"), 20, (width, height))
         #
         # steps = 60
         # for i in range(steps):