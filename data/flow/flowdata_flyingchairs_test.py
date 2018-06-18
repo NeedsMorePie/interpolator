f6542fe10c3790e845536cb2b21ffd849c7fae3e
davis <xdaviswu@gmail.com>
2018-06-18 02:40:11 -0400

    Added flying chairs dataset tests.

--- data/flow/flowdata_flyingchairs_test.py
+++ data/flow/flowdata_flyingchairs_test.py
@@ -6,31 +6,30 @@ import unittest
 from data.flow.flowdata import FlowDataSet
 
 
-class TestSintelFlowDataSet(unittest.TestCase):
+class TestFlyingChairsFlowDataSet(unittest.TestCase):
     def setUp(self):
-        self.data_directory = os.path.join('data', 'flow', 'test_data', 'sintel')
-        self.flow_directory = os.path.join(self.data_directory, 'test_flows')
-        self.image_directory = os.path.join(self.data_directory, 'test_images')
+        self.data_directory = os.path.join('data', 'flow', 'test_data', 'flying_chairs')
         # No data augmentation so that the tests are deterministic.
         self.data_set = FlowDataSet(self.data_directory, batch_size=2, validation_size=1, training_augmentations=False,
-                                    data_source=FlowDataSet.SINTEL)
+                                    data_source=FlowDataSet.FLYING_CHAIRS)
 
         # Test paths.
-        self.expected_image_a_paths = [os.path.join(self.image_directory, 'set_a', 'image_0000.png'),
-                                       os.path.join(self.image_directory, 'set_a', 'image_0001.png'),
-                                       os.path.join(self.image_directory, 'set_a', 'image_0002.png'),
-                                       os.path.join(self.image_directory, 'set_a', 'image_0003.png'),
-                                       os.path.join(self.image_directory, 'set_b', 'image_0001.png')]
-        self.expected_image_b_paths = [os.path.join(self.image_directory, 'set_a', 'image_0001.png'),
-                                       os.path.join(self.image_directory, 'set_a', 'image_0002.png'),
-                                       os.path.join(self.image_directory, 'set_a', 'image_0003.png'),
-                                       os.path.join(self.image_directory, 'set_a', 'image_0004.png'),
-                                       os.path.join(self.image_directory, 'set_b', 'image_0002.png')]
-        self.expected_flow_paths = [os.path.join(self.flow_directory, 'set_a', 'flow_0000.flo'),
-                                    os.path.join(self.flow_directory, 'set_a', 'flow_0001.flo'),
-                                    os.path.join(self.flow_directory, 'set_a', 'flow_0002.flo'),
-                                    os.path.join(self.flow_directory, 'set_a', 'flow_0003.flo'),
-                                    os.path.join(self.flow_directory, 'set_b', 'flow_0001.flo')]
+        full_directory = os.path.join('data', 'flow', 'test_data', 'flying_chairs', 'data')
+        self.expected_image_a_paths = [os.path.join(full_directory, '06530_img1.ppm'),
+                                       os.path.join(full_directory, '06531_img1.ppm'),
+                                       os.path.join(full_directory, '06532_img1.ppm'),
+                                       os.path.join(full_directory, '06533_img1.ppm'),
+                                       os.path.join(full_directory, '22871_img1.ppm')]
+        self.expected_image_b_paths = [os.path.join(full_directory, '06530_img2.ppm'),
+                                       os.path.join(full_directory, '06531_img2.ppm'),
+                                       os.path.join(full_directory, '06532_img2.ppm'),
+                                       os.path.join(full_directory, '06533_img2.ppm'),
+                                       os.path.join(full_directory, '22871_img2.ppm')]
+        self.expected_flow_paths = [os.path.join(full_directory, '06530_flow.flo'),
+                                    os.path.join(full_directory, '06531_flow.flo'),
+                                    os.path.join(full_directory, '06532_flow.flo'),
+                                    os.path.join(full_directory, '06533_flow.flo'),
+                                    os.path.join(full_directory, '22871_flow.flo')]
 
         config = tf.ConfigProto()
         config.gpu_options.allow_growth = True
@@ -55,14 +54,14 @@ class TestSintelFlowDataSet(unittest.TestCase):
         next_images_a, next_images_b, next_flows = self.data_set.get_next_batch()
         images_1_a, images_1_b, flows = self.sess.run([next_images_a, next_images_b, next_flows],
                                                       feed_dict=self.data_set.get_train_feed_dict())
-        self.assertTupleEqual(images_1_a.shape, (2, 436, 1024, 3))
-        self.assertTupleEqual(images_1_b.shape, (2, 436, 1024, 3))
-        self.assertTupleEqual(flows.shape, (2, 436, 1024, 2))
+        self.assertTupleEqual(images_1_a.shape, (2, 384, 512, 3))
+        self.assertTupleEqual(images_1_b.shape, (2, 384, 512, 3))
+        self.assertTupleEqual(flows.shape, (2, 384, 512, 2))
         images_2_a, images_2_b, flows = self.sess.run([next_images_a, next_images_b, next_flows],
                                                       feed_dict=self.data_set.get_train_feed_dict())
-        self.assertTupleEqual(images_2_a.shape, (2, 436, 1024, 3))
-        self.assertTupleEqual(images_2_b.shape, (2, 436, 1024, 3))
-        self.assertTupleEqual(flows.shape, (2, 436, 1024, 2))
+        self.assertTupleEqual(images_2_a.shape, (2, 384, 512, 3))
+        self.assertTupleEqual(images_2_b.shape, (2, 384, 512, 3))
+        self.assertTupleEqual(flows.shape, (2, 384, 512, 2))
 
         # Make sure all the images are different (i.e. read correctly).
         self.assertFalse(np.allclose(images_1_a[0], images_1_a[1]))
@@ -75,9 +74,9 @@ class TestSintelFlowDataSet(unittest.TestCase):
         self.data_set.init_validation_data(self.sess)
         images_a, images_b, flows = self.sess.run([next_images_a, next_images_b, next_flows],
                                                   feed_dict=self.data_set.get_validation_feed_dict())
-        self.assertTupleEqual(images_a.shape, (1, 436, 1024, 3))
-        self.assertTupleEqual(images_b.shape, (1, 436, 1024, 3))
-        self.assertTupleEqual(flows.shape, (1, 436, 1024, 2))
+        self.assertTupleEqual(images_a.shape, (1, 384, 512, 3))
+        self.assertTupleEqual(images_b.shape, (1, 384, 512, 3))
+        self.assertTupleEqual(flows.shape, (1, 384, 512, 2))
         # If init_validation_data isn't called again, validation dataset has hit the end of its epoch and will be out
         # of range.
         end_of_dataset = False
