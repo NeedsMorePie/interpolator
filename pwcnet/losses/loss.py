40c4cf3417fc0da9b873eea9ab255b3efc3fe87f
Sihan Wu <daviswu@mail.cs.toronto.edu>
2018-09-01 17:41:56 -0400

    Updated comments.

--- pwcnet/losses/loss.py
+++ pwcnet/losses/loss.py
@@ -68,7 +68,8 @@ def create_multi_level_unflow_loss(image_a, image_b, forward_flows, backward_flo
                           H_i, W_i for different resolutions). By default the resolutions should be increasing.
     :param backward_flows: List of tensors of shape [B, H_i, W_i, 2]. Flows at different resolutions (i.e. different
                            H_i, W_i for different resolutions). By default the resolutions should be increasing.
-    :param flow_scaling: Float. Scales the expected_flow before comparing it to the flows.
+    :param flow_scaling: Float. Magnitude scale of the forward/backward flows with respect to the final flow. The
+                         forward/backward flows will be divided by this before they are used in this function.
     :param flow_layer_loss_weights: List of floats. Loss weights that correspond to the flows list.
     :param layer_patch_distances: List of ints.
     :param loss_weights: Dict. Key is the loss name and the value is the weight.
